145cb71e526627a75b76a7dbe0d6de914c6a33bb
Drparadox96 <restlessbear96@gmail.com>
2025-09-04 08:50:16 +0000

    Update puma.rb

--- config/puma.rb
+++ config/puma.rb
@@ -1,53 +1,25 @@
-# This configuration file will be evaluated by Puma. The top-level methods that
-# are invoked here are part of Puma's configuration DSL. For more information
-# about methods provided by the DSL, see https://puma.io/puma/Puma/DSL.html.
+# Puma configuration optimized for low-memory Render free instance
 
-# Puma starts a configurable number of processes (workers) and each process
-# serves each request in a thread from an internal thread pool.
-#
-# The ideal number of threads per worker depends both on how much time the
-# application spends waiting for IO operations and on how much you wish to
-# to prioritize throughput over latency.
-#
-# As a rule of thumb, increasing the number of threads will increase how much
-# traffic a given process can handle (throughput), but due to CRuby's
-# Global VM Lock (GVL) it has diminishing returns and will degrade the
-# response time (latency) of the application.
-#
-# The default is set to 3 threads as it's deemed a decent compromise between
-# throughput and latency for the average Rails application.
-#
-# Any libraries that use a connection pool or another resource pool should
-# be configured to provide at least as many connections as the number of
-# threads. This includes Active Record's `pool` parameter in `database.yml`.
-threads_count = ENV.fetch("RAILS_MAX_THREADS") {
-  ENV.fetch("RAILS_ENV", "development") == "development" ? 1 : 5
-}
+# Reduce threads to save memory
+threads_count = ENV.fetch("RAILS_MAX_THREADS") { 1 }  # 1 thread is enough
 threads threads_count, threads_count
 
-# Specifies that the worker count should equal the number of processors in production.
-if ENV["RAILS_ENV"] == "production"
-  require "concurrent-ruby"
-  worker_count = Integer(ENV.fetch("WEB_CONCURRENCY") { Concurrent.physical_processor_count })
-  workers worker_count if worker_count > 1
-else
-  workers 0
-end
+# No workers to avoid forking memory-heavy processes
+workers 0
 
-# Specifies the `worker_timeout` threshold that Puma will use to wait before
-# terminating a worker in development environments.
+# Worker timeout for development
 worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
-# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
+# Port
 port ENV.fetch("PORT", 3000)
 
-# Allow puma to be restarted by `bin/rails restart` command.
+# Allow restart via bin/rails restart
 plugin :tmp_restart
 
-# Specify the PID file. Defaults to tmp/pids/server.pid in development.
-# In other environments, only set the PID file if requested.
+# PID file (optional)
 pidfile ENV["PIDFILE"] if ENV["PIDFILE"]
 
-if ENV.fetch("RUN_SOLID_QUEUE_IN_PUMA") { false } # This is intended for cheap production hosts.
-  plugin :solid_queue                             # In development we always run bin/dev which
-end                                               # uses the Procfile to start solid_queue.
+# Only run SolidQueue if explicitly enabled
+if ENV.fetch("RUN_SOLID_QUEUE_IN_PUMA") { false }
+  plugin :solid_queue
+end
