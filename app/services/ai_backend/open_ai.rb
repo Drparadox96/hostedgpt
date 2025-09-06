03a34c2e597816507577417d1d68bcbe0fb2a495
Drparadox96 <restlessbear96@gmail.com>
2025-09-06 15:07:27 +0000

    Update open_ai.rb

--- app/services/ai_backend/open_ai.rb
+++ app/services/ai_backend/open_ai.rb
@@ -65,9 +65,8 @@ class AIBackend::OpenAI < AIBackend
       parameters: {
         model: @assistant.language_model.api_name,
         messages: system_message(config[:instructions]) + config[:messages],
-        stream: config[:streaming] && @response_handler || nil,
+        stream: nil,
         max_completion_tokens: 2000, # we should really set this dynamically, based on the model, to the max
-        stream_options: config[:streaming] && { include_usage: true } || nil,
         response_format: { type: "text" },
         tools: @assistant.language_model.supports_tools? && Toolbox.tools || nil,
       }.compact.merge(config[:params] || {})
